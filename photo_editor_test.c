194a9e7286b45ff5b38a5edb7f0f0a42fb5dbf78
Shawaiz Haider <shaider.bee23seecs@seecs.edu.pk>
2023-12-15 02:20:00 +0500

    improved it

--- photo_editor_test.c
+++ photo_editor_test.c
@@ -11,8 +11,8 @@ void convertToGrayscale(FIBITMAP* image) {
             RGBQUAD pixel;
             FreeImage_GetPixelColor(image, x, y, &pixel);
 
-            // Convert the pixel to grayscale
-            BYTE gray = (BYTE)((pixel.rgbRed + pixel.rgbGreen + pixel.rgbBlue) / 3);
+            // Convert the pixel to grayscale using luminosity method
+            BYTE gray = (BYTE)(0.21 * pixel.rgbRed + 0.72 * pixel.rgbGreen + 0.07 * pixel.rgbBlue);
 
             pixel.rgbRed = pixel.rgbGreen = pixel.rgbBlue = gray;
 
@@ -21,41 +21,47 @@ void convertToGrayscale(FIBITMAP* image) {
     }
 }
 
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <input_path> <output_path>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input_image_path> <output_image_path>\n", argv[0]);
         return 1;
     }
 
-    const char* inputPath = argv[1];
-    const char* outputPath = argv[2];
+    const char *inputPath = argv[1];
+    const char *outputPath = argv[2];
 
-    // Load an image using FreeImage
-    FIBITMAP* image = FreeImage_Load(FIF_JPEG, inputPath, 0);
-
-    if (!image) {
-        fprintf(stderr, "Failed to load the image: %s\n", inputPath);
-        return 1;
+    FREE_IMAGE_FORMAT fif = FreeImage_GetFileType(inputPath, 0);
+    if (fif == FIF_UNKNOWN) {
+        fif = FreeImage_GetFIFFromFilename(inputPath);
     }
 
-    // Print some debug information about the image
-    unsigned width = FreeImage_GetWidth(image);
-    unsigned height = FreeImage_GetHeight(image);
-    printf("Image loaded successfully: %s\n", inputPath);
-    printf("Image dimensions: %u x %u\n", width, height);
+    if ((fif != FIF_UNKNOWN) && FreeImage_FIFSupportsReading(fif)) {
+        FIBITMAP* image = FreeImage_Load(fif, inputPath, 0);
 
-    // Perform the image processing
-    convertToGrayscale(image);
+        if (!image) {
+            fprintf(stderr, "Failed to load the image: %s\n", inputPath);
+            return 1;
+        }
 
-    // Save the processed image
-    if (FreeImage_Save(FIF_PNG, image, outputPath, 0)) {
-        printf("Image processed successfully. Output saved to: %s\n", outputPath);
+        // Perform the image processing
+        convertToGrayscale(image);
+
+        // Save the processed image
+        if (!FreeImage_Save(FIF_PNG, image, outputPath, 0)) {
+            fprintf(stderr, "Failed to save the image: %s\n", outputPath);
+            FreeImage_Unload(image);
+            return 1;
+        }
+        FREE_IMAGE_TYPE imageType = FreeImage_GetImageType(image);
+        printf("Image Type: %d\n", imageType);
+
+
+        // Unload the image
+        FreeImage_Unload(image);
+        return 0;
     } else {
-        fprintf(stderr, "Failed to save the processed image: %s\n", outputPath);
+        fprintf(stderr, "Unsupported image format.\n");
+        return 1;
     }
-
-    // Unload the image
-    FreeImage_Unload(image);
-
-    return 0;
 }
